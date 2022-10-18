9febab434d7332d38231a7f0e8afbbd830cfc1ad
Evgenuu Sakin <shve-ster@yandex.ru>
2022-10-18 19:55:18 +0300

    nine semi

---
+++ task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+
+
+string GetSequenceDigits(int num)
+{
+    if (num == 1) 
+    {
+        return "1"; 
+    }
+    else
+    {
+        return GetSequenceDigits(num - 1) + " " + num;
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string sequence = GetSequenceDigits(number);
+Console.WriteLine($"Все натуральные числа от {number} до 1: ");
+Console.WriteLine(sequence);
---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+string GetSumSequence(int m, int n)
+{
+    if (n == m) 
+    {
+        return Convert.ToString(m); 
+    }
+    else
+    {
+        return GetSumSequence(m, (n - 1)) + " " + n;
+    }
+}
+
+Console.WriteLine("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = GetSumSequence(m, n);
+Console.WriteLine(result);
