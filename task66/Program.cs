dc60d500092b95665833a52c050574ec78333c5a
Evgenuu Sakin <shve-ster@yandex.ru>
2022-10-19 02:14:32 +0300

    nine semi

--- task66/Program.cs
+++ task66/Program.cs
@@ -2,13 +2,25 @@
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
 
 
+string PrintSequence(int m, int n)
+{
+    if (n == m)
+    {
+        return Convert.ToString(m);
+    }
+    else
+    {
+        return PrintSequence(m, (n - 1)) + " " + n;
+    }
+}
+
+
 int GetSumSequence(int m, int n)
 {
     if (m == n)
     {
         return m;
     }
-
     int sum = m + GetSumSequence(m + 1, n);
     return sum;
 }
@@ -19,6 +31,9 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число n: ");
 int n = Convert.ToInt32(Console.ReadLine());
 
+string result = PrintSequence(m, n); // вывод на экран всех чисел промежутка от м до n 
+Console.WriteLine($"Все числа в промежутке от {m} до {n} : ");
+Console.WriteLine($"{result}");
 
 int sum = GetSumSequence(m, n);
-Console.WriteLine($"Сумма чисел от {m} до {n} = {sum}");
+Console.WriteLine($"Сумма всех чисел в промежутке от {m} до {n} = {sum}");
